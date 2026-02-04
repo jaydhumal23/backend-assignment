f4288f60404fab412e4fcf8c908a11ffbb0baaa1
Jay Dhumal <jaydhumal27@gmail.com>
2026-02-04 22:21:35 +0530

    added render link in cors server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:8080",
+    origin: ["http://localhost:8080", "https://backend-assignment-lf5m.onrender.com"],
     methods: ["GET", "POST", "DELETE", "PATCH"],
 
     credentials: true
